2bbc1d8e1fed34b55509a3829a50bd6de00b9b4d
adityarfw <aditya.rajkumar@wisetail.com>
2022-02-08 14:54:58 -0500

    Fetched meme images from API and stored in Array. Button click will generate a random image url from that array

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,6 +2,31 @@ import React from 'react';
 import memeimg from '../images/memeimg.png';
 
 const Meme = () => {
+  // Fetch Memes and store it in an array
+  // Use useEffect to get Memes array from https://api.imgflip.com/get_memes
+
+  const [memesData, setMemesData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then((res) => res.json())
+      .then((data) => setMemesData(data.data.memes));
+  }, []);
+
+  //console.log(memesData);
+
+  // Get a random meme when button is clicked
+  // DO NOT use Array.map since you dont need an Array back. You just need a single array item
+  const randomMemeGenerator = () => {
+    const randMeme = Math.floor(Math.random() * memesData.length);
+    const imgUrl = memesData[randMeme].url;
+    console.log(imgUrl);
+  };
+
+  const handleClick = () => {
+    randomMemeGenerator();
+  };
+
   return (
     <div className='meme--container'>
       <div className='meme--input'>
@@ -13,7 +38,7 @@ const Meme = () => {
         />
       </div>
       <div className='meme--button'>
-        <button>Get a new meme image ⚠️</button>
+        <button onClick={handleClick}>Get a new meme image ⚠️</button>
       </div>
       <img className='meme--image' src={memeimg} />
     </div>
